002f586aa54d7cfa8871b254e4c4675123d58cca
jujules94 <julienbessodes94@gmail.com>
2017-04-21 12:29:57 +0200

    Add files via upload

---
+++ interactionMatplotlibTkinter.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+ 
+app = tk.Tk()
+app.wm_title("Graphe Matplotlib dans Tkinter")
+ 
+fig = Figure(figsize=(6, 4), dpi=96)
+ax = fig.add_subplot(111)
+ax.plot(range(10), [5, 4, 2, 6, 9, 8, 7, 1, 2, 3])
+ 
+graph = FigureCanvasTkAgg(fig, master=app)
+canvas = graph.get_tk_widget()
+canvas.grid(row=0, column=0)
+ 
+app.mainloop()
